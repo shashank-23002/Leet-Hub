cc71a2cac9f5917d92c9892429f255bf333e6705
Shashank Singh <97145463+shashank-23002@users.noreply.github.com>
2023-05-16 01:28:27 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (53.83%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[' )
+            {
+                st.push(s[i]);
+                continue;
+            }
+            else
+            {
+                if(st.empty())
+                {
+                    return false;
+                }
+                
+                else
+                {
+                    if(s[i]==')')
+                    {
+                        if(st.top()=='(')
+                        {
+                            st.pop();
+                        }
+                        
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    
+                    if(s[i]==']')
+                    {
+                        if(st.top()=='[')
+                        {
+                            st.pop();
+                        }
+                        
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    
+                    if(s[i]=='}')
+                    {
+                        if(st.top()=='{')
+                        {
+                            st.pop();
+                        }
+                        
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        
+        if(st.empty())
+        {
+            return true;
+        }
+        
+        else
+        {
+            return false;
+        }
+    }
+};
