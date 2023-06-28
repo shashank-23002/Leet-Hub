c95881895838653187f6af9f577bf89134fccb24
Shashank Singh <97145463+shashank-23002@users.noreply.github.com>
2023-06-28 12:59:47 +0530

    Time: 289 ms (9.81%), Space: 50.9 MB (13.79%) - LeetHub

--- 0468-validate-ip-address/0468-validate-ip-address.cpp
+++ 0468-validate-ip-address/0468-validate-ip-address.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
-    string validIPAddress(string IP) {
-        regex ipv4("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])"), ipv6("((([0-9a-fA-F]){1,4})\\:){7}([0-9a-fA-F]){1,4}");   // create regex object for regulare expression
-        if(regex_match(IP, ipv4)) // match regex expression with given IP string for IPv4
-            return "IPv4";
-        else if(regex_match(IP, ipv6)) // match regex expression with given IP string for IPv6
+    string validIPAddress(string queryIP) {
+        regex v4("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])");
+            
+            if(regex_match(queryIP,v4)){
+                return "IPv4";
+            }
+        
+        regex v6("((([0-9a-fA-F]){1,4})\\:){7}([0-9a-fA-F]){1,4}");
+        if(regex_match(queryIP,v6))
+        {
             return "IPv6";
-        return "Neither"; // Otherwise return "Neither"
+        }
+        
+        return "Neither";
     }
 };
